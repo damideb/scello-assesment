6c0b71f12f665cc0822a0989ce50a4560de42e7b
Damilola Adebowale <adebzdamzy@gmail.com>
2024-08-22 11:53:28 +0100

    Radio inputs' label

--- src/components/FilterView.vue
+++ src/components/FilterView.vue
@@ -1,32 +1,36 @@
 <template>
   <div class="bg-white w-48 p-5 space-y-5 text-[#25213B] rounded-lg z-50 border-blue500 border">
-    <div class="flex justify-between">
-      <h4>Default</h4>
+    <div class="flex justify-between items-center">
+      <label for="default">
+        Default
+      </label>
       <input
         type="radio"
         name="sortUser"
-       
+       id="default"
         value="default"
         v-model="sortUsers"
         class="p-5 accent-[#6D5BD0] size-4"
       />
     </div>
 
-    <div class="flex justify-between">
-      <h4>First Name</h4>
+    <div class="flex justify-between items-center">
+      <label for="firstName" >First Name</label>
       <input
         type="radio"
         name="sortUser"
+        id="firstName"
         value="firstName"
         v-model="sortUsers"
         class="p-5 accent-[#6D5BD0] size-4"
       />
     </div>
 
-    <div class="flex justify-between">
-      <h4>Last Name</h4>
+    <div class="flex justify-between items-center">
+      <label for="lastName" >Last Name</label>
       <input
         type="radio"
+        id="lastName"
         name="sortUser"
         value="lastName"
         v-model="sortUsers"
@@ -34,10 +38,11 @@
       />
     </div>
 
-    <div class="flex justify-between">
-      <h4>Email</h4>
+    <div class="flex justify-between items-center">
+      <label for="email">Email</label>
       <input
         type="radio"
+        id="email"
         name="sortUser"
         value="email"
         v-model="sortUsers"
